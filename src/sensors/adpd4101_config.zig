187bd0800b17450a9f615e8a6d02ce2e5a661096
billy948787 <billy948787@gmail.com>
2026-01-22 16:11:41 +0800

    change some config to get better data

--- src/sensors/adpd4101_config.zig
+++ src/sensors/adpd4101_config.zig
@@ -2,19 +2,19 @@ const adpd = @import("adpd4101.zig");
 const gpio = @import("../utils/gpio.zig");
 
 pub const oscillator = adpd.Oscillator.INTERNAL_1MHZ;
-pub const timeslot_freq_hz: u32 = 1000;
+pub const timeslot_freq_hz: u32 = 100;
 pub const i2c_device_path = "/dev/i2c-3";
 pub const device_address: u8 = 0x24;
 pub const use_ext_clock = false;
 pub const gpio_id: u32 = 0;
-pub const fifo_threshold: u16 = 6;
+pub const fifo_threshold: u16 = 4;
 pub const time_slots = [_]adpd.TimeSlot{
     .{
         .id = "A",
         .leds = &[_]adpd.Led{
             .{
                 .id = adpd.get_led_id("1A"),
-                .current = 0x0010,
+                .current = 0x07f,
             },
         },
         .data_format = .{
